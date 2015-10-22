a5ab4d8c81cbdaf1c617464127c759e48b7805ae
hirama <hirama.akihiro.ha@gmail.com>
2015-10-22 21:10:36 +0900

    対戦相手周りのクラス追加

---
+++ MonsterSlide/Assets/Scripts/Opponent/AIOpponent.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// シングルモードで戦うAI対戦相手のクラス
+/// </summary>
+public class AIOpponent : BOpponent {
+
+	/// <summary>
+	/// ゲーム開始時のHPが増加する確率閾値(0 ~ 1)
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	public float beginHpUpThreshold;
+
+	/// <summary>
+	/// 現状のHPが増加する確率閾値(0 ~ 1)
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	private float nowHpUpThreshold;
+
+	/// <summary>
+	/// HP増加量
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	public float hpIncrement;
+
+	/// <summary>
+	/// ゲーム開始時のHPが減少する確率閾値(0 ~ 1)
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	public float beginHpDownThreshold;
+
+	/// <summary>
+	/// 現状のHPが減少する確率閾値(0 ~ 1)
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	private float nowHpDownThreshold;
+
+	/// <summary>
+	/// HP減少量
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	public float hpDecrease;
+
+	/// <summary>
+	/// スキル発動確率閾値
+	/// </summary>
+	[Range(0.0f, 1.0f)]
+	public float skillTriggerThreshold;
+
+	// Use this for initialization
+	void Start () {
+		nowHp = 0.0f;
+		nowHpUpThreshold = beginHpUpThreshold;
+		nowHpDownThreshold = beginHpDownThreshold;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// HP増減チェック
+		if (IsHpUp()) { nowHp += hpIncrement; }
+		else if (IsHpDown()) { nowHp -= hpDecrease; }
+
+		// スキル発動チェック
+	}
+
+	/// <summary>
+	/// HPが増加するか
+	/// </summary>
+	private bool IsHpUp()
+	{
+		float check = Random.Range(0.0f, 1.0f);
+		return check > nowHpUpThreshold;
+	}
+
+	/// <summary>
+	/// HPが減少するか
+	/// /// </summary>
+	/// <returns></returns>
+	private bool IsHpDown()
+	{
+		float check = Random.Range(0.0f, 1.0f);
+		return check < nowHpDownThreshold;
+	}
+}
---
+++ MonsterSlide/Assets/Scripts/Opponent/IOpponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 対戦相手のインターフェース
+/// </summary>
+public interface IOpponent {
+
+	/// <summary>
+	/// 対戦相手の現在のHP
+	/// </summary>
+	/// <returns></returns>
+	float GetNowHp();
+}
